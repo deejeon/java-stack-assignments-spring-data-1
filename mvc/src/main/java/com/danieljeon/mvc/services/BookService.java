3e8e1753b4c6fc9a797f660d7958c3c69b1866d8
deejeon <61816883+deejeon@users.noreply.github.com>
2020-06-12 18:54:02 -0700

    initial commit

---
+++ mvc/src/main/java/com/danieljeon/mvc/services/BookService.java
@@ -0,0 +1,50 @@
+package com.danieljeon.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.danieljeon.mvc.models.Book;
+import com.danieljeon.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+	private final BookRepository bookRepository;
+	
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    
+    public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+    	Book optionalBook = this.findBook(id);
+    	optionalBook.setTitle(title);
+    	optionalBook.setDescription(desc);
+    	optionalBook.setLanguage(lang);
+    	optionalBook.setNumberOfPages(numOfPages);
+    	return bookRepository.save(optionalBook);
+    }
+    
+    public void deleteBook(Long id) {
+    	bookRepository.deleteById(id);
+    	return;
+    }
+}
